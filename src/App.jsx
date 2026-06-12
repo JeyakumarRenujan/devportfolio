011cbccd8b39279cc2956de79b20f5f36ef53254
JeyakumarRenujan <renujanrenu5@gmail.com>
2026-06-12 22:03:36 +0530

    feat: create initial responsive navbar layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
-    <div className="h-screen bg-slate-950 flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-cyan-400">
-        DevPortfolio 🚀
-      </h1>
+    <div className="min-h-screen bg-slate-950">
+      <Navbar />
     </div>
   );
 }
