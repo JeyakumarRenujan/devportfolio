a7de783b1ceff65bd2b3a1396ed4134b5a4b0f14
JeyakumarRenujan <renujanrenu5@gmail.com>
2026-06-13 21:10:36 +0530

    error

---
+++ src/data/aboutData.js
@@ -0,0 +1,12 @@
+const aboutData = {
+  title: "About Me",
+
+  description:
+    "I'm a final-year Computer Engineering undergraduate passionate about Full Stack Development and Artificial Intelligence. I enjoy building scalable web applications, solving real-world problems, and continuously learning modern technologies.",
+
+  experience: "1+ Years Learning",
+  projects: "10+ Projects",
+  technologies: "15+ Technologies",
+};
+
+export default aboutData;
